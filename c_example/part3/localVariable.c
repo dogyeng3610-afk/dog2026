abff0727f85b9407addfd49bf2cd637e540a5c82
dogyeong3610-afk <dogyeng3610@gmail.com>
2026-03-13 10:36:59 +0900

    cmake

--- c_example/part3/localVariable.c
+++ c_example/part3/localVariable.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int add(int a, int b); // 선언 
+int add(int a, int b); // 선언
 
 int main(void)
 {
--- c_example/part3/qsort.c
+++ c_example/part3/qsort.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int compare(const void *a, const void *b); // const는 함수 내부에서 상수처럼 쓰겠다라는 의미, void는 원형/ 여러 타입 대응가능, *a는 주소값으로 받아오겠다라는 의미
+int compare(const void *a, const void *b); 
+// const는 함수 내부에서 상수처럼 쓰겠다라는 의미, void는 원형 여러 타입 대응가능, *a는 주소값으로 받아오겠다라는 의미
 // void* 안쪽의 데이터 모름ㅡ맏
 int main(void)
 {
@@ -10,10 +11,12 @@ int main(void)
 
     // qsort(배열, 타입-숫자, 원소의 사이즈(int = 4/sizeof연산자), 비교하는 함수의 타입)
     qsort(nums, indexN, sizeof(int), compare);
+
     for (int i = 0; i < indexN; ++i)
     {
         printf("%d ", nums[i]);
     }
+    
     return 0;
 }
 
