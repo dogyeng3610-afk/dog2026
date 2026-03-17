222692da4032e73c506e6946c89b24859fc0dfd5
dogyeong3610-afk <dogyeng3610@gmail.com>
2026-03-17 17:16:16 +0900

    scoreProcess

---
+++ c_example/part6/scoreProcess/main.c
@@ -0,0 +1,41 @@
+#include "scoreProcess.h"
+
+int main(void)
+{
+    char *finPath = "/home/dog014/dog2026/c_example/part6/scoreProcess/score.dat";
+    char *foutPath = "/home/dog014/dog2026/c_example/part6/scoreProcess/score.out";
+    FILE *fin = fopen(finPath, "r");
+    FILE *fout = fopen(foutPath, "w");
+    int n;
+    printf("처리할 학생의 수를 입력하세요: ");
+    if (scanf("%d", &n) != 1)
+        return 1;
+
+    if (fin == NULL || fout == NULL)
+    {
+        fprintf(stderr, "파일을 열수 없습니다.\n");
+        return 1;
+    }
+
+    Sdata *students = (Sdata *)malloc(sizeof(Sdata) * n);
+    Sdata **table = (Sdata **)malloc(sizeof(Sdata) * n);
+
+    if (students == NULL || table == NULL)
+    {
+        fprintf(stderr, "메모리 할당 실패\n");
+        return 1;
+    }
+
+    inputData(fin, students, n);
+    calculateScore(students, n);
+    calculateRank(students, n);
+    sortPointers(students, table, n);
+    printResult(fout, table, n);
+
+    free(table);
+    free(students);
+
+    fclose(fin);
+    fclose(fout);
+    return 0;
+}
