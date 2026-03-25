8d3911125d96a94e62eaa3795cfcb62fd27a9ac6
dogyeong3610-afk <dogyeng3610@gmail.com>
2026-03-25 11:13:45 +0900

    timerled

--- atmega128/src/timer0led.c
+++ atmega128/src/timer0led.c
@@ -9,15 +9,15 @@ int main()
 {
     DDRC = 0x0F;
 
-    TCCR0 = _BV(CS02) | _BV(CS01) | _BV(CS00); // 분주비
-    TIMSK = _BV(TOIE0);
-    sei();
+    TCCR0 = _BV(CS02) | _BV(CS01) | _BV(CS00); // 분주비 1024
+    TIMSK = _BV(TOIE0); //개별허용
+    sei(); /// 전역 허용
 
     while (1)
     {
-        if (timer0_cnt == 100)
+        if (timer0_cnt == 100)  // 1초마다 if문 작동
         {
-            led_data++;
+            led_data++; // 얼마 만큼의 시간이 흐르면 실행되는가? 얼마에 한번씩 실행되는가? -> 1초 마다 작동
             if (led_data > 0x0F)
                 led_data = 0;
             timer0_cnt = 0;
@@ -30,7 +30,7 @@ int main()
 ISR(TIMER0_OVF_vect)
 {
     cli(); // SREG I 비트 해제
-    TCNT0 = 112;
+    TCNT0 = 112; // 112~255 사이만 카운트 : 144번 카운트 // 1번 카운트 할 때 16M/1024 시간 = 15635 // 1초 -> 15635/133=117
     timer0_cnt++;
     sei(); // 다시 설정
 }
